289666392e7a463620fc77821269c8dad25564ca
jiftechnify <jiftech.stlfy@gmail.com>
2021-12-29 11:19:55 +0900

    Fix: support checking all form of untyped const exprs

--- nakedliteral.go
+++ nakedliteral.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"go/ast"
 	"go/types"
+	"log"
+	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -90,44 +92,128 @@ func processIndexExpr(pass *analysis.Pass, idx *ast.IndexExpr) {
 //
 // `msgfmt` MUST contain exact one format specifier for string(`%s` or `%q`)
 func checkAndReport(pass *analysis.Pass, expr ast.Expr, msgfmt string) {
-	exprPkgPath := pass.Pkg.Path()
-	declType := pass.TypesInfo.Types[expr].Type
+	// no probrem if expr is not constant expression.
+	if pass.TypesInfo.Types[expr].Value == nil {
+		return
+	}
+	// no probrem if expr is not untyped.
+	if !isUntypedConstExpr(pass, expr) {
+		return
+	}
 
-	namedTyp, isNamed := declType.(*types.Named)
+	inferredType := pass.TypesInfo.Types[expr].Type
+
+	namedTyp, isNamed := inferredType.(*types.Named)
 	if !isNamed {
 		return
 	}
-	if !isBasicOrParenedBasic(expr) {
+	if _, isUnderlyingBasic := inferredType.Underlying().(*types.Basic); !isUnderlyingBasic {
 		return
 	}
-	if _, isUnderlyingBasic := declType.Underlying().(*types.Basic); !isUnderlyingBasic {
-		return
-	}
-	// as of this line, (1) the type of expr is declared as Defined Type, (2) the expr is a literal of basic type, and (3) the Underlying Type of declared expr type is basic type
 
 	// expr is target of warning if the declared type of expr is *not* "external package private type"
-	if namedTyp.Obj().Exported() || namedTyp.Obj().Pkg().Path() == exprPkgPath {
+	if namedTyp.Obj().Exported() || namedTyp.Obj().Pkg().Path() == pass.Pkg.Path() {
 		pass.Report(analysis.Diagnostic{
 			Pos:     expr.Pos(),
 			End:     expr.End(),
-			Message: fmt.Sprintf(msgfmt, declType.String()),
+			Message: fmt.Sprintf(msgfmt, inferredType.String()),
 		})
 	}
 }
 
-func isBasicOrParenedBasic(expr ast.Expr) bool {
-	currExpr := expr
-	for {
-		switch e := currExpr.(type) {
-		case *ast.BasicLit:
+// check if `expr` is untyped.
+// precondition: `expr` is constant expression (i.e. has constant value).
+func isUntypedConstExpr(pass *analysis.Pass, expr ast.Expr) bool {
+	// unwrap all parentheses
+	unwrapped := unwrapParens(expr)
+
+	switch e := unwrapped.(type) {
+	case *ast.BasicLit:
+		// Naked basic literals are untyped const expr.
+		return true
+
+	case *ast.Ident:
+		// `true`, `false` and `iota` are untyped const expr.
+		if _, isConst := constIdentNames[e.Name]; isConst {
 			return true
-
-		case *ast.ParenExpr:
-			currExpr = e.X
-			// continue
-
-		default:
+		}
+		// Lookup `types.Object`(type information about entity of code) associated with the ident and check its type.
+		cnst, ok := pass.Pkg.Scope().Lookup(e.Name).(*types.Const)
+		if !ok {
+			// should be unreachable
 			return false
 		}
+		return strings.HasPrefix(cnst.Type().String(), "untyped")
+
+	case *ast.UnaryExpr:
+		// If an operand is untyped, entire expression is also untyped.
+		return isUntypedConstExpr(pass, e.X)
+
+	case *ast.BinaryExpr:
+		// "A constant comparison always yields an untyped boolean constant", as stated in the lang spec.
+		if _, isComparison := comparisonTokens[e.Op.String()]; isComparison {
+			return true
+		}
+		// `expr` is other than comparison. In this case, if both operands are untyped, entire expression is also untyped.
+		return isUntypedConstExpr(pass, e.X) && isUntypedConstExpr(pass, e.Y)
+
+	case *ast.CallExpr:
+		// As stated in the lang spec:
+		// * "Applying the built-in function `complex` to untyped integer, rune, or floating-point constants yields an untyped complex constant".
+		// * "For `real` and `imag`, ... If the argument evaluates to an untyped constant, it must be a number, and the return value of the function is an untyped floating-point constant".
+		// All other call expressions (incl. type conversions) are typed. (any counter examples?)
+		callee, ok := typeutil.Callee(pass.TypesInfo, e).(*types.Builtin)
+		if !ok {
+			return false
+		}
+		if _, isBuiltInAboutComplex := builtInsAboutComplex[callee.Name()]; !isBuiltInAboutComplex {
+			return false
+		}
+
+		// Callee is a built-in function one of `complex`, `real`, `imag`.
+		for _, arg := range e.Args {
+			if !isUntypedConstExpr(pass, arg) {
+				return false
+			}
+		}
+		// All args are untyped!
+		return true
+
+	default:
+		// All other types of expression (index, key-value, selector, slice, star) can't appear in const expr.
+		log.Printf("unexpected node type: %T", e)
+		return false
 	}
+}
+
+func unwrapParens(expr ast.Expr) ast.Expr {
+	currExpr := expr
+	for {
+		paren, isParenExpr := currExpr.(*ast.ParenExpr)
+		if !isParenExpr {
+			return currExpr
+		}
+		currExpr = paren.X
+	}
+}
+
+var constIdentNames = map[string]struct{}{
+	"true":  {},
+	"false": {},
+	"iota":  {},
+}
+
+var comparisonTokens = map[string]struct{}{
+	"==": {},
+	"!=": {},
+	"<":  {},
+	"<=": {},
+	">":  {},
+	">=": {},
+}
+
+var builtInsAboutComplex = map[string]struct{}{
+	"complex": {},
+	"real":    {},
+	"imag":    {},
 }
