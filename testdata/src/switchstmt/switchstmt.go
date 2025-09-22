c004ef81e4bbcff5c16bc073d887b7f2f9997190
jiftechnify <jiftech.stlfy@gmail.com>
2025-09-23 01:46:13 +0900

    improve test

--- testdata/src/switchstmt/switchstmt.go
+++ testdata/src/switchstmt/switchstmt.go
@@ -7,20 +7,77 @@ const (
 	One  ZeroOne = 1
 )
 
-func SwitchStmt(i int, zo ZeroOne) {
+func SwitchStmt(i int, zeroOne ZeroOne) {
 	switch i {
 	case 0:
 	case 1:
 	default:
 	}
 
-	switch zo {
+	switch zeroOne {
 	case Zero:
 	case ZeroOne(1):
 	default:
 	}
 
-	switch zo {
+	switch zeroOne {
+	case 0: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	case 1: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	default:
+	}
+}
+
+func genZeroOne(b bool) ZeroOne {
+	if b {
+		return One
+	}
+	return Zero
+}
+
+func SwitchStmtFuncCall(b bool) {
+	switch genZeroOne(b) {
+	case Zero:
+	case ZeroOne(1):
+	default:
+	}
+
+	switch genZeroOne(b) {
+	case 0: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	case 1: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	default:
+	}
+}
+
+type foo struct {
+	zeroOne ZeroOne
+}
+
+func (f *foo) getZeroOne() ZeroOne {
+	return f.zeroOne
+}
+
+func SwitchStmtSelector(f *foo) {
+	switch f.getZeroOne() {
+	case Zero:
+	case ZeroOne(1):
+	default:
+	}
+
+	switch f.getZeroOne() {
+	case 0: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	case 1: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
+	default:
+	}
+}
+
+func SwitchStmtMethodCall(f *foo) {
+	switch f.getZeroOne() {
+	case Zero:
+	case ZeroOne(1):
+	default:
+	}
+
+	switch f.getZeroOne() {
 	case 0: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
 	case 1: // want `matching untyped constant with expression of defined type "switchstmt.ZeroOne"`
 	default:
